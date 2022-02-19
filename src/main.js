a9cbe3f24d550b7a1d21b2dc5b5e3e806df2f2ec
Destrox <p.kuniczak@gmail.com>
2022-02-19 23:09:06 +0100

    create display and delete features

---
+++ src/main.js
@@ -0,0 +1,50 @@
+const mainObj = {};
+let globalId = 0;
+const optionObj = {"activity": "Aktywność", "work": "Praca", "hobby": "Hobby", "sport": "Sport"}
+const submitBtn = document.getElementById("input-form");
+const select = document.getElementById("category-select");
+
+
+const createTask = (event) => {
+    const tasksList = document.getElementById("tasks-list");
+    event.preventDefault();
+
+    const name = event.target[0].value;
+    const category = optionObj[event.target[2].value];
+    globalId++;
+
+    mainObj[globalId] = {name, category};
+    return tasksList.innerHTML += `<li id=${globalId}> <text> ${name} [${category}]</text>
+    <button class='list-btn' type='button' onclick="deleteTask(this.parentNode.id)"><img src='img/delete.svg' alt='Delete Button'/></button>
+    </li>`;
+}
+
+const createOptions = () => {
+    for (const [key, value] of Object.entries(optionObj)) {
+        const element = `<option value=${key}> ${value}</option>`
+        select.innerHTML += element;
+    }
+}
+
+const deleteTask = (identifier) => {
+    const {id} = document.getElementById(identifier);
+    delete mainObj[id];
+    return displayTasks()
+}
+
+const displayTasks = () => {
+    const tasksListBefore = document.getElementById("tasks-list");
+    tasksListBefore.outerHTML = "<ul id='tasks-list'></ul>";
+    const tasksListAfter = document.getElementById("tasks-list");
+
+    if (Object.keys(mainObj).length > 0)
+        for (const [key, value] of Object.entries(mainObj)) {
+            tasksListAfter.innerHTML += `<li id=${key}> <text> ${value.name} [${value.category}]</text>
+                <button class='list-btn' type='button' onclick="deleteTask(this.parentNode.id)"><img src='img/delete.svg' alt='Delete Button'/></button>
+            </li>`;
+        }
+}
+
+createOptions()
+
+submitBtn.addEventListener("submit", createTask);
